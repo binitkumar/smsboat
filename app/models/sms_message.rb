4ace82a4f4680e1b5c9c9dd6102fdf83f8d32960
Binit Kumar <binit.kumar.hbs@gmail.com>
2017-04-09 01:27:25 +0530

    sms integration

--- app/models/sms_message.rb
+++ app/models/sms_message.rb
@@ -1,4 +1,10 @@
 class SmsMessage < ApplicationRecord
   belongs_to :expert, optional: true
   belongs_to :requester, optional: true
+
+  before_create :send_message
+
+  def send_message
+    TwillioSms.send_message(self.sending_to, self.sending_from, message)
+  end
 end
--- app/models/twilio_sms.rb
+++ app/models/twilio_sms.rb
@@ -1,7 +1,6 @@
 class TwilioSms < ApplicationRecord
 
-  def send_message reciever
-    number_to_send_to = reciever
+  def self.send_message receiver, sender, message
 
     twilio_sid = ENV['TWILIO_SID']
     twilio_token = ENV['TWILIO_TOKEN']
@@ -10,9 +9,9 @@ class TwilioSms < ApplicationRecord
     @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
 
     @twilio_client.account.sms.messages.create(
-      :from => "+1#{twilio_phone_number}",
-      :to => number_to_send_to,
-      :body => "This is an message. It gets sent to #{number_to_send_to}"
+      :from => sernder,
+      :to => receiver,
+      :body => message
     )
   end
 end
